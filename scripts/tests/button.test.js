99b2c0fbec3fd0a107cdb24c609e93e9807d7dd3
nameiswaiyinsin <waiyinsin@gmail.com>
2023-04-01 18:29:16 +0000

    Adda new test to read all h1 tags in index.html via the mock dom in jest, using the beforeEach() directive and fs

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,13 +1,23 @@
+/** * @jest-environment jsdom */
+
 const { TestScheduler } = require("jest");
 const buttonClick = require("../button");
 
+
 beforeEach(() => {        // set up our mock DOM
-    document.body.innerHTML = "<p id='par'></p>"
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);     //gets all h1 tags and stores them into an array, if the h1 tag exists then the length of the array will be 1. If two h1 tags existed then, the the array will be 2, and so on.
     });
 });
